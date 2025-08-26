0ebd1d334ed072f71c3f7d34de1377672f6de25f
rohitpatel-dev <rohitrampatel185@gmail.com>
2025-08-26 23:33:56 +0530

    correction

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import Header from '@/components/Header';
-import Hero from '@/components/Hero';
 import LandingPage from '@/components/Landing';
 import Image from 'next/image';
 
@@ -7,7 +6,7 @@ export default function Home() {
   return (
     <div>
       <Header />
-      {/* <Hero /> */}
+
       <LandingPage />
     </div>
   );
